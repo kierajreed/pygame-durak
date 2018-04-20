cb5b61dbe73f780616ea79620a1556e45667f74e
SkyHawkB <danielmey626@gmail.com>
2018-04-20 16:26:28 -0400

    change stuff around

--- game.py
+++ game.py
@@ -2,26 +2,30 @@ import pygame
 import utils
 import sys
 
-# Initialize pygame and make the window. #
-pygame.init()
-#pygame.display.set_icon(utils.loadImage('logo.png'))
-pygame.display.set_caption('Durak!')
-screen = pygame.display.set_mode(utils.SCREEN_SIZE)
+def main():
+    # Initialize pygame and make the window. #
+    pygame.init()
+    #pygame.display.set_icon(utils.loadImage('logo.png'))
+    pygame.display.set_caption('Durak!')
+    screen = pygame.display.set_mode(utils.SCREEN_SIZE)
 
-deck = utils.getDeckArray(None)
-trump_card = deck.pop(0)
-trump_suit = trump_card.suit
+    deck = utils.getDeckArray(None)
+    trump_card = deck.pop(0)
+    trump_suit = trump_card.suit
 
-deck = utils.getDeckArray(trump_card)
+    deck = utils.getDeckArray(trump_card)
 
-# DEBUG #
-print(deck)
-print('trump_card: ' + str(trump_card))
+    # DEBUG #
+    print(deck)
+    print('trump_card: ' + str(trump_card))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+            screen.fill(utils.BACKGROUND)
+            pygame.display.flip()
 
 # Start the game loop. #
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    screen.fill(utils.BACKGROUND)
-    pygame.display.flip()
+if __name__ == '__main__':
+        main()
