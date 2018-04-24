c1cef2c206428c339a228f59482d2aa36a9531e7
SkyHawkB <danielmey626@gmail.com>
2018-04-24 19:12:33 -0400

    added player methods

--- player.py
+++ player.py
@@ -1,3 +1,9 @@
 class Player(object):
     def __init__(self):
         self.hand = []
+
+    def chooseCardToDefend(self, game_state):
+        raise Exception('Abstract method from Player not implemented in {}!'.format(self.__class__.__name__))
+
+    def chooseCardToAttack(self, game_state):
+        raise Exception('Abstract method from Player not implemented in {}!'.format(self.__class__.__name__))
