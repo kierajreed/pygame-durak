2426d9f0580099def0e9d887792c0f737dbb9380
SkyHawkB <danielmey626@gmail.com>
2018-04-25 21:56:36 -0400

    fixed indentation...

--- game.py
+++ game.py
@@ -34,17 +34,16 @@ if __name__ == '__main__':
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
 
-                screen.fill(utils.BACKGROUND)
+            screen.fill(utils.BACKGROUND)
 
-                screen.blit(utils.loadTrumpCard(trump_card), utils.TRUMP_POSITION)
-                screen.blit(utils.loadCardBack(), utils.DECK_POSITION)
+            screen.blit(utils.loadTrumpCard(trump_card), utils.TRUMP_POSITION)
+            screen.blit(utils.loadCardBack(), utils.DECK_POSITION)
 
-                for index in range(0, len(players[0].hand)):
-                    screen.blit(utils.loadCard(players[0].hand[index]), utils.getCardPosition(index, len(players[0].hand), False))
-                for index in range(0, len(players[1].hand)):
-                    screen.blit(utils.loadCardBack(), utils.getCardPosition(index, len(players[1].hand), True))
+            for index in range(0, len(players[0].hand)):
+                screen.blit(utils.loadCard(players[0].hand[index]), utils.getCardPosition(index, len(players[0].hand), False))
+            for index in range(0, len(players[1].hand)):
+                screen.blit(utils.loadCardBack(), utils.getCardPosition(index, len(players[1].hand), True))
+            if utils.STATUS != None:
+                screen.blit(utils.getStatusMessage(), utils.STATUS_POSITON)
 
-                if utils.STATUS != None:
-                    screen.blit(utils.getStatusMessage(), utils.STATUS_POSITON)
-
-                pygame.display.flip()
+            pygame.display.flip()
